d4e1f0a9f37e7a83157d70357966eb76b83f6daf
Jacob Barrow <jacob.barrow@gmail.com>
2019-10-22 13:51:53 -0500

    create pseudocode

--- js/uno.js
+++ js/uno.js
@@ -1,20 +1,41 @@
 
 
 /* creation of variables*/
-
-let player1Hand = [];
-let player2Hand = [];
-let player3Hand = [];
-let discard = [];
-
+var player1Hand = [];
+var player2Hand = [];
+var player3Hand = [];
+var discard = [];
 
 
+let players = [
+    player1Hand,
+    player2Hand,
+    player3Hand
+];
 
-function assignCard(randomNum) {
 
+
+/* ===============BEGIN UNO PSEUDOCODE
+
+    -for loop to loop through each person for entirety of game
+        -for loop to issue out random cards to each person
+            -for loop as each player draws a card
+            -for loop as each player decides on card to play
+=================================== */
+
+function deal() {
+
+    for (let i = 0; i < 4; i++) {
+        let randomNum = (Math.floor(Math.random() * 54 - 1) + 2);
+        player1Hand.unshift(randomNum);
+        randomNum = (Math.floor(Math.random() * 54 - 1) + 2);
+        player2Hand.unshift(randomNum);
+        randomNum = (Math.floor(Math.random() * 54 - 1) + 2);
+        player3Hand.unshift(randomNum);
+
+    }
 }
-
-
-
-
-
+deal();
+console.log(player1Hand);
+console.log(player2Hand);
+console.log(player3Hand);
