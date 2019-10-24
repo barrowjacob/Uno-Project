be37a6ac9f505d1a8a7ebff40785598c9f448c76
Jacob Barrow <jacob.barrow@gmail.com>
2019-10-24 07:56:52 -0500

    i've given up on uno

--- js/uno.js
+++ js/uno.js
@@ -1,24 +1,31 @@
 
 
-/* creation of variables*/
-let player1Hand = [];
-let player2Hand = [];
-let player3Hand = [];
-let discard = [];
-let players = [
+
+/**************************/
+// creation of variables //
+/*************************/
+
+var player1Hand = [];
+var player2Hand = [];
+var player3Hand = [];
+var discard = [];
+var players = [
     player1Hand,
     player2Hand,
     player3Hand
 ];
 
 
-let randomItem = function () {
+var randomItem = function () {
     return deck[Math.floor(Math.random() * deck.length)];
 };
-// ============ first DEAL =============== //
+
+/******************************************/
+// ============ INTIAL DEAL =============== //
+/******************************************/
 
 function deal() {
-    for (let i = 0; i < 4; i++) {
+    for (var i = 0; i < 4; i++) {
         randomItem();
         player1Hand.unshift(randomItem());
         randomItem();
@@ -28,7 +35,6 @@ function deal() {
       }
 }
 
-// actual deal with function below //
 deal();
 
 // check the players hands//
@@ -36,68 +42,81 @@ console.log(player1Hand);
 console.log(player2Hand);
 console.log(player3Hand);
 
-// =============== begin first ROUND ============== //
+/*************************************************************/
+// ============ begin iterating through players =========== //
+/************************************************************/
 
 
         discard = deck[Math.floor(Math.random() * deck.length)];
 
+        /*************************/
         // first in discard pile //
-        console.log("this is the discard " + discard.value + " and " + discard.color);
+        /*************************/
 
+        console.log("The starting discard is: " + discard.value + " " + discard.color);
+
+        /****************************/
         // loop through each player //
+        /****************************/
+
         var i = 0;
         while (player1Hand.length !== 0 && player2Hand.length !== 0 && player3Hand.length !== 0) {
             for (i = 0; i < players.length; i++) {
                 for (var j = 0; j < players[i].length; j++) {
-                    //if the player has the matching card //
-                    if (players[i][j].value === discard.value || players[i][j].color === discard.color) {
-                        console.log(players[i][j].value + " " + players[i][j].color + " This worked!");
-                        discard = players[i][j];
-                        console.log("this is the new discard " + discard.color + " " + discard.value);
-                        players[i].splice(players[i][j], 1);
-                        console.log("this is now the player's hand" + players[i]);
-
-
-                        // if the player has a wild card //
-                    } else if (players[i][j].name.includes("wildCard")) {
-                        console.log("this is the new discard " + discard.name);
-
-
-                        //if the player doesn't have the card -- going to make them just skip that turn//
+                    if (player1Hand.length === 0 || player2Hand.length === 0 || player3Hand.length === 0) {
+                        break;
                     } else {
-                        console.log(players[i][j].value + " " + players[i][j].color + " Shoot.");
+
+                        /***********************************************************/
+                        // ========= if the player has the matching card ========= //
+                        /***********************************************************/
+
+                        if (players[i][j].value === discard.value || players[i][j].color === discard.color) {
+                            console.log("Player " + i + " played a " + players[i][j].value + " " + players[i][j].color + "! This worked!");
+                            console.log("=========");
+                            discard = players[i][j];
+                            players[i].splice(players[i][j], 1);
+                            if (players[i].length < 1) {
+                                console.log("Player " + i + " wins!!");
+                                break;
+                            } else {
+                                console.log("Player " + i + " now has " + players[i].length + " cards!");
+                                console.log("=========");
+                            }
+
+                            /***********************************************************/
+                            // =========== if the player has a wild card ============= //
+                            /***********************************************************/
+
+                        } else if (players[i][j].name.includes("wildCard")) {
+                            console.log("Player " + i + " discarded a " + discard.color + " " + discard.value);
+                            console.log("=========");
+
+                            /*************************************************************/
+                            //========== if the player doesn't have the card ===========//
+                            /*************************************************************/
+
+                        } else if (players[i][j] !== (players[i].length - 1)) {
+                            // THE LINE BELOW CREATES AN INFINITE LOOP ??!!?? //
+                            console.log("Player " + i + " tried to play a " + players[i][j].value + " " + players[i][j].color + " to no avail. Trying another card.");
+                            console.log("=========");
+                            for (var y = 0; y < players[i][j].length; y++) {
+                                if (players[i][j].value !== discard.value || players[i][j].color !== discard.color) {
+                                    players[i].unshift(randomItem());
+
+                                }
+                            }
+                            console.log("Player " + i + " had to draw another card. They got a " + players[i][j].color + " " + players[i][j].value);
+
+                        } else {
+                                console.log("Player " + i + " has no cards available. Moving to next player.");
+                            console.log("=========");
+                                break;
+                        }
                     }
                 }
             }
         }
-
-        //repeat the steps for the next player //
-
-        // for (i = 0; i < 4; i ++) {
-        //     if (player2Hand[i].value === discard.value || player2Hand[i].color === discard.color) {
-        //         console.log(player2Hand[i].value + " " + player2Hand[i].color + " This worked!");
-        //         discard = player2Hand[i];
-        //         console.log("this is the new discard " + discard.color + " " + discard.value);
-        //         break;
-        //     }else if (player1Hand[i].name.includes("wildCard") || player1Hand[i].name.includes("wildCard_also") || player1Hand[i].name.includes("wildDrawFour") || player1Hand[i].name.includes("wildDrawFour_also")) {
-        //         discard = player1Hand[i];
-        //         console.log("this is the new discard " + discard.name);
-        //     } else {
-        //         console.log(player2Hand[i].value + " " + player2Hand[i].color + " Shoot.");
-        //     }
-        // }for (i = 0; i < 4; i ++) {
-        //     if (player3Hand[i].value === discard.value || player3Hand[i].color === discard.color) {
-        //         console.log(player3Hand[i].value + " " + player3Hand[i].color + " This worked!")
-        //         discard = player3Hand[i];
-        //         console.log("this is the new discard " + discard.color + " " + discard.value);
-        //         break;
-        //     } else if (player1Hand[i].name.includes("wildCard") || player1Hand[i].name.includes("wildCard_also") || player1Hand[i].name.includes("wildDrawFour") || player1Hand[i].name.includes("wildDrawFour_also")) {
-        //         discard = player1Hand[i];
-        //         console.log("this is the new discard " + discard.name);
-        //     } else {
-        //         console.log(player3Hand[i].value + " " + player3Hand[i].color + " Shoot.");
-        //     }
-        // }
 
 
 
