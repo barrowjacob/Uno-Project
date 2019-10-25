e8b526af5d877b51a3c767cfb25a1720832e16e5
Ethan Joiner <ethan.joiner@gmail.com>
2019-10-24 19:34:31 -0500

    colors randomized

--- js/joiner_week_one.js
+++ js/joiner_week_one.js
@@ -3,41 +3,52 @@
 
 // BOM DOT DOM
 
-//
-//
-// var d1 = document.getElementById("d1");
-//
-// var d2 = document.getElementById("d2");
-//
-// var d3 = document.getElementById("d3");
-//
-// var d4 = document.getElementById("d4");
-//
-// var d5 = document.getElementById("d5");
-//
-// var d6 = document.getElementById("d6");
-//
-// var d7 = document.getElementById("d7");
-//
-// var d8 = document.getElementById("d8");
-//
-// var d9 = document.getElementById("d9");
-//
-// var d10 = document.getElementById("d10");
-//
-// function sizeChange() {
-//
-//     for (var i = 0; i < array.length; i++) {
-//         var randomNumber = Math.floor(Math.random() * 45) + 10;
-//         array[i].style.fontSize = randomNumber + "px";
-//         console.log("Changing size")
-//     }
-// }
-//
-// setInterval(sizeChange, 500);
-// var array = [d1, d2, d3, d4, d5, d6, d7, d8, d9, d10];
 
 
+var d1 = document.getElementById("d1");
+
+var d2 = document.getElementById("d2");
+
+var d3 = document.getElementById("d3");
+
+var d4 = document.getElementById("d4");
+
+var d5 = document.getElementById("d5");
+
+var d6 = document.getElementById("d6");
+
+var d7 = document.getElementById("d7");
+
+var d8 = document.getElementById("d8");
+
+var d9 = document.getElementById("d9");
+
+var d10 = document.getElementById("d10");
+
+function sizeChange() {
+
+    for (var i = 0; i < array.length; i++) {
+        var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        var randomNumber = Math.floor(Math.random() * 45) + 10;
+        array[i].style.fontSize = randomNumber + "px";
+        array[i].style.color = "#" + randomColor;
+        console.log("Changing size")
+    }
+}
+
+setInterval(sizeChange, 500);
+var array = [d1, d2, d3, d4, d5, d6, d7, d8, d9, d10];
+
+
+// Number sort
+function filterNumbers(x) {
+    var newArray = x.filter(n => typeof n === "number" && !isNaN(n));
+    newArray.sort(function(a, b) {
+        return a - b;
+    });
+    return newArray;
+}
+console.log(filterNumbers([1, true, "four", 3, 5, 2]));
 
 // Working at Jimmy Johns Again
 var breads  = [
@@ -69,6 +80,9 @@ function makeSandwichObjects(breads, fillings) {
 }
 
 console.log(makeSandwichObjects(breads, fillings));
+
+// Link for Codeup Exercises
+// https://java.codeup.com/extra-exercises/javascript/arrays-and-objects/
 
 
 
