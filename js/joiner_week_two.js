8a67dcff366516dac120235306eeb0b7925ede6f
Ethan Joiner <ethan.joiner@gmail.com>
2019-10-28 15:41:14 -0500

    true 1 line

--- js/joiner_week_two.js
+++ js/joiner_week_two.js
@@ -1,5 +1,2 @@
-var fizzbuzz = function (x) {
-    for (var n = 1; n < x; n++) { n % 15 === 0 ? console.log("fizzbuzz") : n % 3 === 0 ? console.log("fizz") : n % 5 === 0 ? console.log("buzz") : console.log(n)
-    }};
-
+var fizzbuzz = function (x) { for (var n = 1; n < x; n++) { n % 15 === 0 ? console.log("fizzbuzz") : n % 3 === 0 ? console.log("fizz") : n % 5 === 0 ? console.log("buzz") : console.log(n)}};
 fizzbuzz(100);
