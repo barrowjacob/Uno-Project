d7fa38ba728f599bfa71e784998111005d60d13b
Ethan Joiner <ethan.joiner@gmail.com>
2019-11-01 09:08:51 -0500

    interval destroyed

--- js/joiner_week_two.js
+++ js/joiner_week_two.js
@@ -1,14 +1,11 @@
 "use strict";
+
 $(document).ready(function() {
 
 //One Line Fizzbuzz and Ternary Practice
 
-    var fizzbuzz = function (x) {
-        for (var n = 1; n < x; n++) {
-            n % 15 === 0 ? console.log("fizzbuzz") : n % 3 === 0 ? console.log("fizz") : n % 5 === 0 ? console.log("buzz") : console.log(n)
-        }
-    };
-    fizzbuzz(100);
+    var fizzbuzz = function (x) {for (var n = 1; n < x; n++) {n % 15 === 0 ? console.log("fizzbuzz") : n % 3 === 0 ? console.log("fizz") : n % 5 === 0 ? console.log("buzz") : console.log(n)}};
+    // fizzbuzz(100);
 
 
 //Color and Size with Jquery
@@ -33,7 +30,8 @@ $(document).ready(function() {
 
     var d10 = document.getElementById("d10");
 
-    var clicked = 0
+    var clicked = 0;
+
 
     $('#container').children().click(function() {
         var randomColor = Math.floor(Math.random() * 16777215).toString(16);
@@ -42,16 +40,30 @@ $(document).ready(function() {
         $(this).css("color", '#' + randomColor).css('font-size', randomSize).html(randomNumber)
     });
 
-    $('h1').click(function() {
-
-        $(this).next().children().each(function () {
+    function randomize() {
+        $('div').first().children().each(function () {
             var randomColor = Math.floor(Math.random() * 16777215).toString(16);
             var randomSize = Math.floor(Math.random() * 45) + 10;
             var randomNumber = Math.floor(Math.random() * 10);
             $(this).css("color", '#' + randomColor).css('font-size', randomSize).html(randomNumber)
-        })
+        });
+    }
+    var randomInterval;
+    $('h1').click(function() {
+        randomize();
+        clicked++;
+        if (clicked === 10) {
+            alert("Let me save you the trouble");
+            randomInterval = setInterval(randomize, 300);}
 
-    })
+            if (clicked > 10) {
+                alert("As you wish.");
+                clearInterval(randomInterval);
+                clicked = 0;
 
+        }
+        console.log(clicked);
+    });
 
 });
+
